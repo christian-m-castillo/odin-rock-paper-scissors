bec22443583ebd7879b340d904306aabd45696fb
christian-m-castillo <christian.castillo.email@gmail.com>
2024-05-30 17:27:31 -0500

    Added playRound function

--- js/app.js
+++ js/app.js
@@ -14,5 +14,57 @@ function getHumanChoice() {
   return humanChoice;
 }
 
-console.log(`computer choice is ${getComputerChoice()}`);
-console.log(`human choice is ${getHumanChoice()}`);
+
+let computerScore = 0;
+let humanScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  let roundEvalMessage;
+  switch (humanChoice) {
+    case 'rock': 
+      if (computerChoice == 'rock') {
+        roundEvalMessage = 'Tie!';
+        return roundEvalMessage;
+      } else if (computerChoice == 'paper') {
+        roundEvalMessage = 'Paper covers rock: you lose!';
+        computerScore++;
+        return roundEvalMessage;
+      } else if (computerChoice == 'scissors') {
+        roundEvalMessage = 'Rock smashes scissors: you win!';
+        humanScore++;
+        return roundEvalMessage;
+      }
+    case 'paper': 
+      if (computerChoice == 'rock') {
+        roundEvalMessage = 'Paper covers rock: you win!';
+        humanScore++;
+        return roundEvalMessage;
+      } else if (computerChoice == 'paper') {
+        roundEvalMessage = 'Tie!';
+        return roundEvalMessage;
+      } else if (computerChoice == 'scissors') {
+        roundEvalMessage == 'Scissors cuts paper: you lose!';
+        computerChoice++;
+      }
+    case 'scissors':
+      if (computerChoice == 'rock') {
+        roundEvalMessage = 'Rock smashes scissors: you lose!';
+        computerScore++;
+        return roundEvalMessage;
+      } else if (computerChoice == 'paper') {
+        roundEvalMessage = 'Scissors cuts paper: you win!';
+        humanScore++;
+        return roundEvalMessage;
+      } else if (computerChoice == 'scissors') {
+        roundEvalMessage = 'Tie!';
+        return roundEvalMessage;
+      }
+  } 
+}
+playRound();
+// Round 1
+getHumanChoice();
+let result = playRound(getHumanChoice(), getComputerChoice());
+console.log(result);
+console.log(`Your score: ${humanScore}, computer score: ${computerScore}`);
+//Round 2
